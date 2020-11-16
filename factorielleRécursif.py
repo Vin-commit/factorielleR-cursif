e17d74205f7ae357a7720d2cfd109170d48e0543
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 18:58:43 +0100

    Add files via upload

---
+++ factorielleRécursif.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+# Le nombre d'appels récursifs possibles dépend de la valeur de n.
+def factorial(n):
+  if (n == 1):
+    return 1
+  # else: // remplace la dernière ligne de la fonction.
+  #  return n * factorial (n-1)
+  return n * factorial(n-1)
+
+
+try:
+  n = int(input("Entrez un nombre dont on veut la factorielle : "))
+  print("Sa factorielle est :", factorial(n))
+except:
+  print(“Veuillez saisir un nombre.”)
+)e
+
+
+----------------------------------------------------------------------- Résultat ---------------------------------------------------------------------------
+Entrez un nombre dont on veut la factorielle : 5
+Sa factorielle est : 120
+~                                                                        
+Sa
+~
